0f8e859d5eff6f1955b6415a1f5bf9b9657b8519
Diego A <168478166+ComidaRapida007@users.noreply.github.com>
2026-06-16 17:15:41 -0500

    feat(main): setup application bootstrapping, theme, and global blocs

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:synhub/requests/bloc/request_bloc.dart';
+import 'package:synhub/requests/services/request_service.dart';
+import 'package:synhub/shared/views/Login.dart';
+import 'package:synhub/tasks/bloc/task/task_bloc.dart';
+
+import 'shared/bloc/auth/auth_bloc.dart';
+import 'shared/bloc/member/member_bloc.dart';
+import 'shared/services/auth_service.dart';
+import 'shared/services/member_service.dart';
+import 'tasks/services/task_service.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => AuthBloc(
+            authService: AuthService(),
+            memberService: MemberService(),
+          ),
+        ),
+        BlocProvider(
+          create: (_) => MemberBloc(memberService: MemberService()),
+        ),
+        BlocProvider(
+          create: (_) => TaskBloc(taskService: TaskService()),
+        ),
+        BlocProvider(
+          create: (_) => RequestBloc(requestService: RequestService()),
+        )
+      ],
+      child: MaterialApp(
+        title: 'SynHub',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: Login(),
+      ),
+    );
+  }
+}
